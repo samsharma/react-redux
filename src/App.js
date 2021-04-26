378cd5f23bf8d65cc22547cee726bfc27d7e7a4a
samsharma <sambhu.tyco@gmail.com>
2021-04-27 04:33:41 +0530

    reacr redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Provider} from 'react-redux';
+
+//components
+
+import BookContainer from './components/bookContainer';
+import HookBookContainer from './components/HookBookContainer';
+import store from './redux/store';
+
 import './App.css';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <BookContainer></BookContainer>
+      <HookBookContainer></HookBookContainer>
     </div>
+    </Provider>
   );
 }
 
